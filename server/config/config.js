213cc1ebe693524fc9dba331260d35390330c452
Jorge Caro <jorgeluiscaro@gmail.com>
2020-04-23 19:19:07 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://trukwa:GjRndr2gdAEfNlD2@cluster0-tmxb5.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
