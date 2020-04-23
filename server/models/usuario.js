4e29970b7b41ae59c89de558e00a5fd32e013351
Jorge Caro <jorgeluiscaro@gmail.com>
2020-04-23 18:38:13 -0400

    RestServer - Usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    messege: '{VALUE} no es un rol valido'
+};
+
+let Schema = mongoose.Schema;
+
+let usuarioScheme = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'el correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'la contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+usuarioScheme.method.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+usuarioScheme.plugin(uniqueValidator, {
+    messege: '{PATH} debe se ser unico'
+});
+
+module.exports = mongoose.model('usuario', usuarioScheme);
---
+++ server/routes/usuario.js
@@ -0,0 +1,172 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+
+
+app.get('/usuario', function(req, res) {
+    //res.json('get usuario LOCAL')
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde); //combertir en número un String
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre email role estado google img') // para agregar exclusiones o filtrado se ingresan los campos a mostrar
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+
+
+            })
+
+
+
+        });
+
+
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+
+    });
+
+    // if (body.nombre === undefined) {
+
+    //     res.status(400).json({
+    //         ok: false,
+    //         mensaje: 'El nombre es necesario'
+    //     });
+    // } else {
+
+    //     res.json({
+    //         persona: body
+    //     })
+    // }
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    })
+
+});
+
+app.delete('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let cambiaEstado = {
+        estado: false
+    }
+
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioEstado) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (!usuarioEstado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioEstado
+        });
+
+
+        // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+        //     if (err) {
+        //         return res.status(400).json({
+        //             ok: false,
+        //             err
+        //         });
+        //     };
+
+        //     if (!usuarioBorrado) {
+        //         return res.status(400).json({
+        //             ok: false,
+        //             err: {
+        //                 message: 'Usuario no encontrado'
+        //             }
+        //         });
+
+        //     }
+
+        //     res.json({
+        //         ok: true,
+        //         usuario: usuarioBorrado
+        //     });
+
+        // });
+
+
+    });
+});
+
+
+module.exports = app;
